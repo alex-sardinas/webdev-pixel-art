a4d848b34fa0122d00c994d260f76d7d9b1acdf7
Alexander Sardinas <21345@columbushs.com>
2019-09-17 12:56:13 -0400

    Pixel Art

---
+++ script.js
@@ -0,0 +1,14 @@
+var penColor = 'black';
+
+function setPixelColor(pixel){
+    pixel.style.backgroundColor = 'black';
+}
+
+
+function setPenColor(pen){
+    penColor = pen;
+}
+
+function setPixelColor(pixel){
+    pixel.style.backgroundColor = penColor;
+}
